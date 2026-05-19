85d6079d4460c4d0f59ced116faf619f686d4513
chiba <daikon.yeah@gmail.com>
2026-05-20 01:04:53 +0900

    Add files via upload

---
+++ src/training/run_training_loop.py
@@ -0,0 +1,90 @@
+best_metric = -1
+best_metric_epoch = -1
+best_metrics_epochs_and_time = [[], [], []]
+epoch_loss_values = []
+metric_values = []
+metric_values_tc = []
+metric_values_wt = []
+metric_values_et = []
+
+total_start = time.time()
+for epoch in range(max_epochs):
+    epoch_start = time.time()
+    print("-" * 10)
+    print(f"epoch {epoch + 1}/{max_epochs}")
+    model.train()
+    epoch_loss = 0
+    step = 0
+    for batch_data in train_loader:
+        step_start = time.time()
+        step += 1
+        inputs, labels = (
+            batch_data["image"].to(device),
+            batch_data["label"].to(device),
+        )
+        optimizer.zero_grad()
+        with torch.cuda.amp.autocast():
+            outputs = model(inputs)
+            loss = loss_function(outputs, labels)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        epoch_loss += loss.item()
+        print(
+            f"{step}/{len(train_ds) // train_loader.batch_size}"
+            f", train_loss: {loss.item():.4f}"
+            f", step time: {(time.time() - step_start):.4f}"
+        )
+    lr_scheduler.step()
+    epoch_loss /= step
+    epoch_loss_values.append(epoch_loss)
+    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+    if (epoch + 1) % val_interval == 0:
+        model.eval()
+        with torch.no_grad():
+
+            for val_data in val_loader:
+                val_inputs, val_labels = (
+                    val_data["image"].to(device),
+                    val_data["label"].to(device),
+                )
+                val_outputs = inference(val_inputs)
+                val_outputs = [post_trans(i) for i in decollate_batch(val_outputs)]
+                dice_metric(y_pred=val_outputs, y=val_labels)
+                dice_metric_batch(y_pred=val_outputs, y=val_labels)
+
+            metric = dice_metric.aggregate().item()
+            metric_values.append(metric)
+            metric_batch = dice_metric_batch.aggregate()
+            metric_tc = metric_batch[0].item()
+            metric_values_tc.append(metric_tc)
+            metric_wt = metric_batch[1].item()
+            metric_values_wt.append(metric_wt)
+            metric_et = metric_batch[2].item()
+            metric_values_et.append(metric_et)
+            dice_metric.reset()
+            dice_metric_batch.reset()
+            print("validating case:", val_data["image"].meta["filename_or_obj"])
+
+            if metric > best_metric:
+                best_metric = metric
+                best_metric_epoch = epoch + 1
+                best_metrics_epochs_and_time[0].append(best_metric)
+                best_metrics_epochs_and_time[1].append(best_metric_epoch)
+                best_metrics_epochs_and_time[2].append(time.time() - total_start)
+                torch.save(
+                    model.state_dict(),
+                    os.path.join(root_dir, "best_metric_model.pth"),
+                )
+                print("saved new best metric model")
+            print(
+                f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
+                f" tc: {metric_tc:.4f} wt: {metric_wt:.4f} et: {metric_et:.4f}"
+                f"\nbest mean dice: {best_metric:.4f}"
+                f" at epoch: {best_metric_epoch}"
+            )
+    print(f"time consuming of epoch {epoch + 1} is: {(time.time() - epoch_start):.4f}")
+total_time = time.time() - total_start
+
+print(f"train completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}, total time: {total_time}.")
